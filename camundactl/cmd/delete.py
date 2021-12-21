01af7459f6eefc06bf2e2397848d8c6c08d75262
Jens Blawatt <jens.blawatt@codecentric.de>
2021-12-21 15:49:44 +0100

    fix: add get_factory_method to delete command

--- camundactl/cmd/delete.py
+++ camundactl/cmd/delete.py
@@ -1,5 +1,10 @@
 from camundactl.cmd.get import OpenAPIMulitCommandBase
+from camundactl.cmd.openapi.factory import OpenAPICommandFactory
 
 
 class DeleteMultiCommand(OpenAPIMulitCommandBase):
     verb = "delete"
+
+    def get_factory_method(self, factory: OpenAPICommandFactory):
+        return factory.create_delete_command
+
